7ce80b650de536978f94b48b53bcd3c82d75d132
Arbaz Ajaz <azz@technologies.dk>
2024-01-29 11:42:56 +0500

    dnd files & folders in navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import {
+  FaFile,
   FaFolder,
+  FaAngleDown,
+  FaFolderOpen,
   FaAngleRight,
   FaFolderPlus,
   FaFileCirclePlus,
-  FaAngleDown,
-  FaFile,
-  FaFolderOpen,
 } from "react-icons/fa6";
+import {
+  FC,
+  useRef,
+  useState,
+  useEffect,
+  ComponentProps,
+  PropsWithChildren,
+} from "react";
 import { produce } from "immer";
-import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
-import { ComponentProps, FC, PropsWithChildren } from "react";
-import { atomFamily, selectAtom } from "jotai/utils";
-import { Disclosure } from "@headlessui/react";
 import { tv } from "tailwind-variants";
+import { atomFamily, selectAtom } from "jotai/utils";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
 
 enum EItem {
   FILE = "FILE",
@@ -38,6 +46,7 @@ type IItem = IFile | IFolder;
 type IStore = {
   rootId: string;
   openedFolderId: string;
+  dragItemId: string | null;
   children: { [id: string]: IItem };
 };
 
@@ -47,6 +56,7 @@ const rootId = generateId();
 
 const storeAtom = atom<IStore>({
   rootId,
+  dragItemId: null,
   openedFolderId: rootId,
   children: {
     [rootId]: {
@@ -66,6 +76,16 @@ const openedFolderIdAtom = atom(
       storeAtom,
       produce(get(storeAtom), (draft) => {
         draft.openedFolderId = folderId;
+      }),
+    ),
+);
+const dragItemIdAtom = atom(
+  (get) => get(storeAtom).dragItemId,
+  (get, set, folderId: string | null) =>
+    set(
+      storeAtom,
+      produce(get(storeAtom), (draft) => {
+        draft.dragItemId = folderId;
       }),
     ),
 );
@@ -104,6 +124,59 @@ const createItemAtom = atom(null, (get, set, type: EItem) => {
 
   set(storeAtom, update);
 });
+
+const moveAtom = atom(
+  null,
+  (
+    get,
+    set,
+    props: {
+      dragItemId: string;
+      dropItemId: string;
+      hovering: "above" | "below";
+    },
+  ) => {
+    const { dragItemId, dropItemId, hovering } = props;
+    const store = get(storeAtom);
+
+    const update = produce(store, (draft) => {
+      const dragItem = draft.children[dragItemId];
+      const dragItemParent = draft.children[
+        dragItem.parentId || draft.rootId
+      ] as IFolder;
+      const dropItem = draft.children[dropItemId];
+      const dropItemParent = draft.children[
+        dropItem.parentId || draft.rootId
+      ] as IFolder;
+
+      // unlink from parent
+      const dragItemIndex = dragItemParent.childrenIds.indexOf(dragItemId);
+      dragItemParent.childrenIds.splice(dragItemIndex, 1);
+
+      if (dropItem.type === EItem.FOLDER && hovering === "below") {
+        // link to new parent
+        if (!dropItem.childrenIds.includes(dragItemId))
+          dropItem.childrenIds.unshift(dragItemId);
+
+        // assign new parent
+        dragItem.parentId = dropItem.id;
+      } else {
+        const dropItemIndex = dropItemParent.childrenIds.indexOf(dropItemId);
+
+        const newIndex =
+          hovering === "above" ? dropItemIndex : dropItemIndex + 1;
+
+        if (newIndex < 0) dropItemParent.childrenIds.unshift(dragItemId);
+        else dropItemParent.childrenIds.splice(newIndex, 0, dragItemId);
+
+        // assign new parent
+        dragItem.parentId = dropItemParent.id;
+      }
+    });
+
+    set(storeAtom, update);
+  },
+);
 
 const Header: FC<{ title: string } & PropsWithChildren> = ({
   title,
@@ -155,45 +228,130 @@ const navigatorItem = tv({
     selected: {
       true: "bg-gray-300 hover:bg-gray-300",
     },
+    hide: {
+      true: "opacity-0",
+    },
   },
 });
 
+const useDisclosure = (defaultValue = false) => {
+  const [isOpen, setIsOpen] = useState(defaultValue);
+  const close = () => setIsOpen(false);
+  const open = () => setIsOpen(true);
+  const toggle = () => setIsOpen((pv) => !pv);
+
+  return {
+    open,
+    close,
+    toggle,
+    isOpen,
+  };
+};
+
+interface IDragPayload {
+  itemId: string;
+}
+
 const NavigatorItem: FC<{ itemId: string }> = ({ itemId }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
   const item = useAtomValue(itemFamily(itemId));
+  const [dragItemId, setDragItemId] = useAtom(dragItemIdAtom);
   const [selectedFolder, selectFolder] = useAtom(openedFolderIdAtom);
 
+  const hide = dragItemId === item.id;
+
   const selected = selectedFolder === item.id;
+
+  const { isOpen, close, open, toggle } = useDisclosure(selected);
+
+  const move = useSetAtom(moveAtom);
+
+  const [{ isDragging }, connectDrag] = useDrag(
+    () => ({
+      item: { itemId: item.id } satisfies IDragPayload,
+      type: item.type,
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    }),
+    [item.type, item.id],
+  );
+
+  useEffect(() => {
+    setDragItemId(isDragging ? item.id : null);
+  }, [isDragging, item.id, setDragItemId]);
+
+  const [, connectDrop] = useDrop(
+    () => ({
+      accept: [EItem.FILE, EItem.FOLDER],
+      hover: (dragItem: IDragPayload, monitor) => {
+        const dragItemId = dragItem.itemId;
+
+        if (dragItemId === item.id) return;
+
+        const draggingOffset = monitor.getClientOffset();
+        if (!draggingOffset) return;
+
+        const { y } = draggingOffset;
+
+        const dropRect = ref.current?.getBoundingClientRect();
+
+        if (!dropRect) return;
+
+        if (item.type === EItem.FOLDER) {
+          open();
+        }
+
+        const hovering =
+          y < dropRect.height / 2 + dropRect.y ? "above" : "below";
+
+        move({ dragItemId, dropItemId: item.id, hovering });
+      },
+    }),
+    [item, open, move],
+  );
+
+  useEffect(() => {
+    if (isDragging) {
+      close();
+    }
+  }, [close, isDragging]);
+
+  connectDrop(connectDrag(ref));
 
   if (!item) return <></>;
 
   if (item.type === EItem.FILE)
     return (
-      <div className={navigatorItem({})}>
+      <div ref={ref} className={navigatorItem({ hide })}>
         <FaFile /> <span className="flex-1 text-left">{item.id}.file</span>
       </div>
     );
 
   if (item.type === EItem.FOLDER)
     return (
-      <Disclosure defaultOpen={selected}>
-        {({ open }) => (
-          <div className="w-full">
-            <Disclosure.Button
-              onClick={() => selectFolder(item.id)}
-              className={navigatorItem({ selected })}
-            >
-              {selected ? <FaFolderOpen /> : <FaFolder />}{" "}
-              <span className="flex-1 text-left">{item.id}</span>
-              {open ? <FaAngleDown /> : <FaAngleRight />}
-            </Disclosure.Button>
-            <Disclosure.Panel className="ml-2">
-              {item.childrenIds.map((childId) => (
-                <NavigatorItem itemId={childId} />
-              ))}
-            </Disclosure.Panel>
+      <div className="w-full">
+        <div
+          ref={ref}
+          onClick={() => {
+            toggle();
+            selectFolder(item.id);
+          }}
+          className={navigatorItem({ selected, hide })}
+        >
+          {selected ? <FaFolderOpen /> : <FaFolder />}{" "}
+          <span className="flex-1 text-left">{item.id}</span>
+          {isOpen ? <FaAngleDown /> : <FaAngleRight />}
+        </div>
+        {isOpen && (
+          <div className="flex flex-col ml-2">
+            {item.childrenIds.map((childId) => (
+              <NavigatorItem key={childId} itemId={childId} />
+            ))}
           </div>
         )}
-      </Disclosure>
+      </div>
     );
 
   return <></>;
@@ -202,16 +360,19 @@ const NavigatorItem: FC<{ itemId: string }> = ({ itemId }) => {
 const Navigator: FC = () => {
   const rootId = useAtomValue(rootIdAtom);
   return (
-    <div className="bg-gray-100 w-1/3 h-full flex flex-col">
-      <Header title="Navigator" />
-      <div className="flex flex-col overflow-auto py-4">
-        <NavigatorItem itemId={rootId} />
+    <DndProvider backend={HTML5Backend}>
+      <div className="bg-gray-100 w-1/3 h-full flex flex-col">
+        <Header title="Navigator" />
+        <div className="flex flex-col overflow-auto py-4">
+          <NavigatorItem itemId={rootId} />
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
 const App: FC = () => {
+  // console.debug(JSON.stringify(useAtomValue(storeAtom), null, 2));
   const openedFolderId = useAtomValue(openedFolderIdAtom);
   return (
     <div className="w-[100svw] h-[100svh] bg-gray-50 flex items-center justify-center">
